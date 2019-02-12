e10f90b638ffb314b47273f1d94c50d82d5b0f59
Jorgen Kvalvaag <jorgen.kvalvaag@nordicsemi.no>
2019-02-12 13:07:14 +0100

    WIP

--- peripheral/src/main.c
+++ peripheral/src/main.c
@@ -21,8 +21,8 @@
 #include <bluetooth/conn.h>
 #include <bluetooth/uuid.h>
 #include <bluetooth/gatt.h>
+#include <gpio.h>
 
-#include <pwm.h>
 static struct gpio_callback gpio_cb;
 
 /* Custom Service Variables */
@@ -55,10 +55,7 @@ static ssize_t write_vnd(struct bt_conn *conn, const struct bt_gatt_attr *attr,
 	u8_t *pointer = (u8_t*)buf;
 	u32_t pwm_value = pointer[0] | (pointer[1] << 8);
 	printk("written: %d\n", pwm_value);
-	int err = pwm_pin_set_usec(pwm, 8,
-			   20000, 1800);
-	k_delayed_work_submit(&work, 500);
-	printk("err: %d\n", err);
+	
 	if (offset + len > sizeof(vnd_value)) {
 		return BT_GATT_ERR(BT_ATT_ERR_INVALID_OFFSET);
 	}
@@ -99,16 +96,22 @@ static void connected(struct bt_conn *conn, u8_t err)
 		printk("Connected\n");
 	}
 	bt_le_adv_stop();
+	struct device *dev = device_get_binding(LED0_GPIO_CONTROLLER);
+	gpio_pin_write(dev, LED0_GPIO_PIN, 0);
 }
 
 static void disconnected(struct bt_conn *conn, u8_t reason)
 {
 	printk("Disconnected (reason %u)\n", reason);
-	err = bt_le_adv_start(BT_LE_ADV_CONN_NAME, ad, ARRAY_SIZE(ad), NULL, 0);
+	k_cpu_idle();
+	/*int err = bt_le_adv_start(BT_LE_ADV_CONN_NAME, ad, ARRAY_SIZE(ad), NULL, 0);
 	if (err) {
 		printk("Advertising failed to start (err %d)\n", err);
 		return;
 	}
+	*/
+	struct device *dev = device_get_binding(LED0_GPIO_CONTROLLER);
+	gpio_pin_write(dev, LED0_GPIO_PIN, 1);
 }
 
 static struct bt_conn_cb conn_callbacks = {
@@ -124,13 +127,13 @@ static void bt_ready(int err)
 	}
 
 	printk("Bluetooth initialized\n");
-
+	bt_conn_cb_register(&conn_callbacks);
 	bt_gatt_service_register(&vnd_svc);
-
+	
 	if (IS_ENABLED(CONFIG_SETTINGS)) {
 		settings_load();
 	}
-
+	/*
 	err = bt_le_adv_start(BT_LE_ADV_CONN_NAME, ad, ARRAY_SIZE(ad), NULL, 0);
 	if (err) {
 		printk("Advertising failed to start (err %d)\n", err);
@@ -138,24 +141,43 @@ static void bt_ready(int err)
 	}
 
 	printk("Advertising successfully started\n");
+	*/
+	//bt_le_adv_start(BT_LE_ADV_CONN_NAME, ad, ARRAY_SIZE(ad), NULL, 0);
+
+}
+
+void button_pressed(struct device *port,
+					struct gpio_callback *cb,
+					u32_t pins)
+{
+	//struct device *dev = device_get_binding(LED0_GPIO_CONTROLLER);
+	///gpio_pin_write(dev, LED0_GPIO_PIN, 0);
+	bt_le_adv_start(BT_LE_ADV_CONN_NAME, ad, ARRAY_SIZE(ad), NULL, 0);
+	struct device *dev = device_get_binding(LED0_GPIO_CONTROLLER);
+	gpio_pin_write(dev, LED0_GPIO_PIN, 1);
+
 }
 
 void main(void)
 {
 	int err;
-	struct device *gpiob = device_get_binding("GPIO_0");
+	struct device *gpiob = device_get_binding(SW0_GPIO_CONTROLLER);
 	if (!gpiob) {
 		printk("error\n");
 		return;
 	}
+	gpio_pin_configure(gpiob, SW0_GPIO_PIN,
+			   GPIO_DIR_IN | GPIO_INT | GPIO_PUD_PULL_UP | GPIO_INT_EDGE);
 
-	gpio_pin_configure(gpiob, 11,
-			   GPIO_DIR_IN | GPIO_INT |  GPIO_PUD_PULL_UP | GPIO_INT_EDGE);
-
-	gpio_init_callback(&gpio_cb, button_pressed, BIT(11));
+	gpio_init_callback(&gpio_cb, button_pressed, BIT(SW0_GPIO_PIN));
 
 	gpio_add_callback(gpiob, &gpio_cb);
-	gpio_pin_enable_callback(gpiob, 11);
+	gpio_pin_enable_callback(gpiob, SW0_GPIO_PIN);
+
+	struct device *dev = device_get_binding(LED0_GPIO_CONTROLLER);
+
+	gpio_pin_configure(dev, LED0_GPIO_PIN, GPIO_DIR_OUT);
+	gpio_pin_write(dev, LED0_GPIO_PIN, 0);
 
 	err = bt_enable(bt_ready);
 	if (err) {
@@ -163,9 +185,12 @@ void main(void)
 		return;
 	}
 
-	bt_conn_cb_register(&conn_callbacks);
+	
 
 	while (1) {
 		k_sleep(MSEC_PER_SEC);
+		//gpio_pin_write(dev, LED0_GPIO_PIN, 0);
+		k_sleep(MSEC_PER_SEC);
+		//gpio_pin_write(dev, LED0_GPIO_PIN, 0);
 	}
 }
